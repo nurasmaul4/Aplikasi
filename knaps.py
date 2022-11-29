3df7182d3ab52e9b6fab303c61241a70c3bff953
nurasmaul4 <112039369+nurasmaul4@users.noreply.github.com>
2022-11-29 13:06:14 +0700

    Update knaps.py

--- knaps.py
+++ knaps.py
@@ -14,7 +14,7 @@ from sklearn.datasets import make_classification
 from sklearn.svm import SVC
 
 st.write(""" 
-# Cek data
+# Negara Bijaksana
 """)
 
 st.write("=========================================================================")
