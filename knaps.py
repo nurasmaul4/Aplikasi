57e5f64f3fc0c64b679b54b51e74fba538d11ce8
nurasmaul4 <112039369+nurasmaul4@users.noreply.github.com>
2022-12-01 06:10:55 +0700

    Update knaps.py

--- knaps.py
+++ knaps.py
@@ -47,178 +47,244 @@ with Deskripsi_dataset:
 
 with preporcessing:
     st.write("""# Preprocessing""")
-    df[["gender", "age", "hypertension", "heart_disease", "ever_married", "work_type", "Residence_type", "avg_glucose_level", "smoking_status"]].agg(['min','max'])
+    import numpy as np
+    import pandas as pd
+    import matplotlib.pyplot as plt
+    %matplotlib inline
+    import seaborn as sns
+    import plotly.express as px
+    import plotly.graph_objects as go
+    from sklearn.impute import SimpleImputer
+    # Basic packages
+    import pandas as pd, numpy as np, matplotlib.pyplot as plt, seaborn as sns, gc
+    from scipy import stats; from scipy.stats import zscore, norm, randint
+    import matplotlib.style as style; style.use('fivethirtyeight')
 
-    df.stroke.value_counts()
-    df = df.drop(columns=["id","bmi"])
+    # Display settings
+    pd.options.display.max_rows = 400
+    pd.options.display.max_columns = 100
+    pd.options.display.float_format = "{:.2f}".format
 
-    X = df.drop(columns="stroke")
-    y = df.stroke
-    "### Hapus fitur yang tidak diperlukan"
-    df
-
-    le = preprocessing.LabelEncoder()
-    le.fit(y)
-    y = le.transform(y)
-
-    "### Transformasi Label"
-    y
-
-    le.inverse_transform(y)
-
-    labels = pd.get_dummies(df.stroke).columns.values.tolist()
-
-    "### Label"
-    labels
-
-    st.markdown("# Normalize")
-
-    "### Normalize data"
-
-    dataubah=df.drop(columns=['gender','ever_married','work_type','Residence_type','smoking_status'])
-    dataubah
-
-    "### Normalize data gender"
-    data_gen=df[['gender']]
-    gen = pd.get_dummies(data_gen)
-    gen
-
-    # "### Normalize data Hypertension"
-    # data_hypertension=df[['hypertension']]
-    # hypertension = pd.get_dummies(data_hypertension)
-    # hypertension
-
-    "### Normalize data married"
-    data_married=df[['ever_married']]
-    married = pd.get_dummies(data_married)
-    married
-
-    "### Normalize data work"
-    data_work=df[['work_type']]
-    work = pd.get_dummies(data_work)
-    work
-
-    "### Normalize data residence"
-    data_residence=df[['Residence_type']]
-    residence = pd.get_dummies(data_residence)
-    residence
-
-    "### Normalize data smoke"
-    data_smoke=df[['smoking_status']]
-    smoke = pd.get_dummies(data_smoke)
-    smoke
-
-    dataOlah = pd.concat([gen,married,work,residence,smoke], axis=1)
-    dataHasil = pd.concat([df,dataOlah], axis = 1)
-
-    X = dataHasil.drop(columns=["gender","ever_married","work_type","Residence_type","smoking_status","stroke"])
-    y = dataHasil.stroke
-    "### Normalize data hasil"
-    X
-
-    scaler = MinMaxScaler()
-    scaler.fit(X)
-    X = scaler.transform(X)
-    "### Normalize data transformasi"
-    X
-
-    X.shape, y.shape
-
-    le.inverse_transform(y)
-
-    labels = pd.get_dummies(dataHasil.stroke).columns.values.tolist()
+    random_state = 42
+    np.random.seed(random_state)
     
-    "### Label"
-    labels
+    # Suppress warnings
+    import warnings; warnings.filterwarnings('ignore')
 
-    # """## Normalisasi MinMax Scaler"""
+    data = pd.read_csv('../input/malnutrition-across-the-globe/malnutrition-estimates.csv')
+    data_by_country = pd.read_csv('../input/malnutrition-across-the-globe/country-wise-average.csv')
+    data.drop(['Unnamed: 0','ISO code','Survey Year','Source','Report Author','Notes','Short Source'], axis=1, inplace=True)
 
+    def income_map(val):
+        mapper = {0:'Low Income', 1:'Lower Middle Income', 2:'Upper Middle Income',3:'High Income'}
+        return mapper[val]
+    def lldc_map(val):
+        mapper = {0:'Others', 2:'SIDS', 1:'LLDC'}
+        return mapper[val]
 
-    scaler = MinMaxScaler()
-    scaler.fit(X)
-    X = scaler.transform(X)
-    X
+    data['Income Classification'] = data['Income Classification'].apply(income_map)
+    data['LLDC or SID2'] = data['LLDC or SID2'].apply(lldc_map)
+    
+    data.head()
+    
+    data.columns
+    
+    data.info()
+    
+    data.describe().T
+    
+    "# Check missing values in the dataframe"
+    data.isnull().sum()
+    
+    columns = list(['Severe Wasting', 'Wasting','Overweight', 'Stunting', 'Underweight'])
 
-    X.shape, y.shape
+    print('Descriptive Stats before imputation for columns with missing values: \n', '--'*35)
+    display(data[columns].describe().T)
 
+    data['Wasting'].fillna(data['Wasting'].mean(), inplace=True)
+    data['Severe Wasting'].fillna(data['Severe Wasting'].mean(), inplace=True)
+    data['Overweight'].fillna(data['Overweight'].mean(), inplace=True)
+    data['Stunting'].fillna(data['Stunting'].mean(), inplace=True)
+    data['Underweight'].fillna(data['Underweight'].mean(), inplace=True)
 
+    print('Descriptive Stats after imputation: \n', '--'*35)
+    display(data[columns].describe().T)
+    
+    "#Univariate Analysis"
+    # Functions that will help us with EDA plot
+    def odp_plots(df, col):
+    f,(ax1, ax2, ax3) = plt.subplots(1, 3, figsize = (15, 7.2))
+    
+    # Boxplot to check outliers
+    sns.boxplot(x = col, data = df, ax = ax1, orient = 'v', color = 'darkslategrey')
+    
+    # Distribution plot with outliers
+    sns.distplot(df[col], ax = ax2, color = 'teal', fit = norm).set_title(f'{col} with outliers')
+    
+    # Removing outliers, but in a new dataframe
+    upperbound, lowerbound = np.percentile(df[col], [1, 99])
+    y = pd.DataFrame(np.clip(df[col], upperbound, lowerbound))
+    
+    # Distribution plot without outliers
+    sns.distplot(y[col], ax = ax3, color = 'tab:orange', fit = norm).set_title(f'{col} without outliers')
+    
+    kwargs = {'fontsize':14, 'color':'black'}
+    ax1.set_title(col + ' Boxplot Analysis', **kwargs)
+    ax1.set_xlabel('Box', **kwargs)
+    ax1.set_ylabel(col + ' Values', **kwargs)
+
+    return plt.show()
+    
+    "#Distribution plots"
+    # Outlier, distribution for columns with outliers
+    boxplotcolumns = ['Severe Wasting', 'Wasting', 'Overweight', 'Stunting',
+                      'Underweight']
+    for cols in boxplotcolumns:
+        Q3 = data[cols].quantile(0.75)
+        Q1 = data[cols].quantile(0.25)
+        IQR = Q3 - Q1
+
+        print(f'{cols.capitalize()} column', '--'*40)
+        count = len(data.loc[(data[cols] < (Q1 - 1.5 * IQR)) | (data[cols] > (Q3 + 1.5 * IQR))])
+        print(f'no of records with outliers values: {count}')
+
+        display(data.loc[(data[cols] < (Q1 - 1.5 * IQR)) | (data[cols] > (Q3 + 1.5 * IQR))].head())
+        print(f'EDA for {cols.capitalize()} column', '--'*40)
+        odp_plots(data, cols)
+
+    del cols, IQR, boxplotcolumns
+    
+    "#Multivariate Analysis"
+    corr = data.corr()
+    mask = np.zeros_like(corr, dtype = np.bool)
+    mask[np.triu_indices_from(mask)] = True
+    sns.heatmap(corr, mask = mask,  linewidths = .5 )#, annot = True)
+    
+    # Filter for correlation value greater than threshold
+    sort = corr.abs().unstack()
+    sort = sort.sort_values(kind = "quicksort", ascending = False)
+    display(sort[(sort > 0.7) & (sort < 1)])
+    
+    "#Negara mana yang menunjukkan persentase Underweight tertinggi? ---> Bangladesh"
+    country = data.loc[:,['Country','Underweight']]
+    country['percunder'] = country.groupby('Country')['Underweight'].transform('max')
+    country = country.drop('Underweight',axis=1).drop_duplicates().sort_values('percunder', ascending=False).head()
+
+    fig = px.pie(country, names='Country', values='percunder', template='seaborn')
+    fig.update_traces(rotation=90, pull=[0.2,0.03,0.1,0.03,0.1], textinfo="percent+label", showlegend=False)
+    fig.show()
+    
+    "#Negara mana yang menunjukkan persentase Kegemukan tertinggi? ---> Albania"
+    country = data.loc[:,['Country','Overweight']]
+    country['percunder'] = country.groupby('Country')['Overweight'].transform('max')
+    country = country.drop('Overweight',axis=1).drop_duplicates().sort_values('percunder', ascending=False).head()
+
+    fig = px.pie(country, names='Country', values='percunder', template='seaborn')
+    fig.update_traces(rotation=90, pull=[0.2,0.03,0.1,0.03,0.1], textinfo="percent+label", showlegend=False)
+    fig.show()
+    
+    "#Kelas pendapatan mana yang memiliki persentase underweight tertinggi? ---> Pendapatan Menengah Bawah"
+    f,(ax1, ax2, ax3) = plt.subplots(1, 3, figsize = (15, 7.2))
+    sns.distplot(data['Underweight'], ax=ax1)
+
+    df_LM = data.loc[data['Income Classification'] == 'Lower Middle Income']
+    df_UM = data.loc[data['Income Classification'] == 'Upper Middle Income']
+    df_Low = data.loc[data['Income Classification'] == 'Low Income']
+    df_High = data.loc[data['Income Classification'] == 'High Income']
+
+    sns.distplot( df_LM['Underweight'],ax = ax2 , color = 'r')
+    sns.distplot( df_UM['Underweight'],ax = ax2, color = 'g')
+    sns.distplot( df_Low['Underweight'],ax = ax2, color = 'b')
+    sns.distplot( df_High['Underweight'],ax = ax2, color = 'y')
+    
+    df = data.loc[:,['Income Classification','Underweight']]
+    df['maxunder'] = df.groupby('Income Classification')['Underweight'].transform('mean')
+    df = df.drop('Underweight', axis=1).drop_duplicates()
+    df = data.loc[:,['Income Classification','Underweight']]
+    df['maxunder'] = df.groupby('Income Classification')['Underweight'].transform('mean')
+    df = df.drop('Underweight', axis=1).drop_duplicates()
+
+    fig = sns.barplot(data=df, x='Income Classification', y='maxunder')
+    fig.set(xticklabels = ['LM', 'UM', 'Low', "High"])
+    plt.show()
+    
+    df = data.loc[:,['Income Classification','Underweight']]
+    df['maxunder'] = df.groupby('Income Classification')['Underweight'].transform('max')
+    df = df.drop('Underweight', axis=1).drop_duplicates()
+
+    fig = px.pie(df, names='Income Classification', values='maxunder', template='seaborn')
+    fig.update_traces(rotation=90, pull=0.05, textinfo="percent+label", showlegend=False)
+    fig.show()
+    
+    "#Persentase underweight di Negara Terbelakang vs Negara Maju"
+    f,(ax1, ax2, ax3) = plt.subplots(1, 3, figsize = (15, 7.2))
+    df_with_LDC = data.loc[data['LDC'] == 1]
+    df_with_DC = data.loc[data['LDC'] == 0]
+
+    sns.distplot(data['Underweight'], ax=ax1)
+    sns.distplot( df_with_LDC['Underweight'],ax = ax2 , color = 'r')
+    sns.distplot( df_with_DC['Underweight'],ax = ax2, color = 'g')
+
+    df = data.loc[:,['LIFD','Underweight']]
+    df['maxunder'] = df.groupby('LIFD')['Underweight'].transform('mean')
+    df = df.drop('Underweight', axis=1).drop_duplicates()
+
+    fig = sns.barplot(data=df, x='LIFD', y='maxunder', ax=ax3)
+    fig.set(xticklabels = ['Not LIFD', 'LIFD'])
+    plt.show()
+    
+    "#Persentase Negara Kekurangan Pangan Berpenghasilan Rendah"
+    f,(ax1, ax2, ax3) = plt.subplots(1, 3, figsize = (15, 7.2))
+    df_with_LIFD = data.loc[data['LIFD'] == 1]
+    df_with_NLIFD = data.loc[data['LIFD'] == 0]
+
+    sns.distplot(data['Underweight'], ax=ax1)
+    sns.distplot( df_with_LIFD['Underweight'],ax = ax2 , color = 'r')
+    sns.distplot( df_with_NLIFD['Underweight'],ax = ax2, color = 'g')
+
+    df = data.loc[:,['LIFD','Underweight']]
+    df['maxunder'] = df.groupby('LIFD')['Underweight'].transform('mean')
+    df = df.drop('Underweight', axis=1).drop_duplicates()
+    df = data.loc[:,['LIFD','Underweight']]
+    df['maxunder'] = df.groupby('LIFD')['Underweight'].transform('mean')
+    df = df.drop('Underweight', axis=1).drop_duplicates()
+
+    fig = sns.barplot(data=df, x='LIFD', y='maxunder')
+    fig.set(xticklabels = ['Not LIFD', 'LIFD'])
+    plt.show()
+    
+    "#Analisis Underweight menurut Kelompok Pendapatan"
+    data["Income Classification"].value_counts()
+    
+    "#Negara Berkembang yang Terkurung Daratan vs Negara Berkembang Pulau Kecil vs Lainnya ---> Lainnya"
+    df = data.loc[:,['LLDC or SID2','Underweight']]
+    df['maxunder'] = df.groupby('LLDC or SID2')['Underweight'].transform('max')
+    df = df.drop('Underweight', axis=1).drop_duplicates()
+
+    fig = px.pie(df, names='LLDC or SID2', values='maxunder', template='seaborn')
+    fig.update_traces(rotation=90, pull=0.05, textinfo="percent+label", showlegend=False)
+    fig.show()
+    
+    "#Severe Wasting"
+    "#Ini adalah % anak usia 0–59 bulan yang berada di bawah minus tiga standar deviasi dari rata-rata berat badan terhadap tinggi badan"
+    sns.kdeplot(data=data['Severe Wasting'], shade=True)
+    plt.title('Distribution of Sever Wasting percentages in countries')
+    plt.show()
+    
+    st.write("Kita dapat melihat dari plot distribusi bahwa nilai persentase o setinggi 12% di beberapa negara. Dari scatter plot dapat diketahui bahwa persentase wasting parah yang tinggi sebagian besar ditemukan untuk ukuran sampel survei yang rendah.")
+    
+    "#Severe Wasting - Wasting - Overweight - Underweight"
+    sns.pairplot(data[['Severe Wasting','Overweight','Underweight', 'Stunting']])
+    plt.show()
+    
+    "#penduduk negara tersebut yang berusia di bawah 5 tahun"
+    sns.kdeplot(data=data['U5 Population (\'000s)'], shade=True)
+    plt.title('Distribution of U5 Population')
+    plt.show()
+    
 
 with modeling:
-    X_train,X_test,y_train,y_test = train_test_split(X,y,test_size=0.2,random_state=4)
-    from sklearn.preprocessing import StandardScaler
-    sc = StandardScaler()
-    X_train = sc.fit_transform(X_train)
-    X_test = sc.transform(X_test)
-    # from sklearn.feature_extraction.text import CountVectorizer
-    # cv = CountVectorizer()
-    # X_train = cv.fit_transform(X_train)
-    # X_test = cv.fit_transform(X_test)
-    st.write("""# Modeling """)
-    st.subheader("Berikut ini adalah pilihan untuk Modeling")
-    st.write("Pilih Model yang Anda inginkan untuk Cek Akurasi")
-    naive = st.checkbox('Naive Bayes')
-    kn = st.checkbox('K-Nearest Neighbor')
-    des = st.checkbox('Decision Tree')
-    mod = st.button("Modeling")
-
-    # NB
-    GaussianNB(priors=None)
-
-    # Fitting Naive Bayes Classification to the Training set with linear kernel
-    nvklasifikasi = GaussianNB()
-    nvklasifikasi = nvklasifikasi.fit(X_train, y_train)
-
-    # Predicting the Test set results
-    y_pred = nvklasifikasi.predict(X_test)
-    
-    y_compare = np.vstack((y_test,y_pred)).T
-    nvklasifikasi.predict_proba(X_test)
-    akurasi = round(100 * accuracy_score(y_test, y_pred))
-    # akurasi = 10
-
-    # KNN 
-    K=10
-    knn=KNeighborsClassifier(n_neighbors=K)
-    knn.fit(X_train,y_train)
-    y_pred=knn.predict(X_test)
-
-    skor_akurasi = round(100 * accuracy_score(y_test,y_pred))
-
-    # DT
-
-    dt = DecisionTreeClassifier()
-    dt.fit(X_train, y_train)
-    # prediction
-    dt.score(X_test, y_test)
-    y_pred = dt.predict(X_test)
-    #Accuracy
-    akurasiii = round(100 * accuracy_score(y_test,y_pred))
-
-    if naive :
-        if mod :
-            st.write('Model Naive Bayes accuracy score: {0:0.2f}'. format(akurasi))
-    if kn :
-        if mod:
-            st.write("Model KNN accuracy score : {0:0.2f}" . format(skor_akurasi))
-    if des :
-        if mod :
-            st.write("Model Decision Tree accuracy score : {0:0.2f}" . format(akurasiii))
-    
-    eval = st.button("Evaluasi semua model")
-    if eval :
-        # st.snow()
-        source = pd.DataFrame({
-            'Nilai Akurasi' : [akurasi,skor_akurasi,akurasiii],
-            'Nama Model' : ['Naive Bayes','KNN','Decision Tree']
-        })
-
-        bar_chart = alt.Chart(source).mark_bar().encode(
-            y = 'Nilai Akurasi',
-            x = 'Nama Model'
-        )
-
-        st.altair_chart(bar_chart,use_container_width=True)
-
+   
 # with modeling:
 
 #     st.markdown("# Model")
