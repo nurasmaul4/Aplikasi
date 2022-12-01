0a873c2b969a72b6db7c22e8a38e3d913a23834f
nurasmaul4 <112039369+nurasmaul4@users.noreply.github.com>
2022-12-01 10:41:10 +0700

    Update knaps.py

--- knaps.py
+++ knaps.py
@@ -92,7 +92,6 @@ with preporcessing:
     
     from sklearn.tree import DecisionTreeClassifier
     dtree=DecisionTreeClassifier(max_depth=3)
-    dtree.fit(xx,y)
     dtree.score(xx,y)
     omanprediction=dtree.predict([[1.68,7.783333,3.55,16.066667,11.916667,332.156]])
     print(float(omanprediction))
